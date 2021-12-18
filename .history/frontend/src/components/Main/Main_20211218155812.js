c1bb2bf2c68cde2ee41a52cada60ab03d0221007
yosefalan <yosefalan@DESKTOP-SU3N0TR.localdomain>
2021-12-18 17:43:56 -0600

    merge issues resolved

---
+++ .history/frontend/src/components/Main/Main_20211218155812.js
@@ -0,0 +1,22 @@
+import React from "react"
+import Navigation from '../Navigation/Navigation'
+import styles from "./Main.module.css"
+
+
+function Main() {
+  return (
+    <>
+      <Navigation />
+      <div className={styles.main}>
+        <img
+        className={styles.banner}
+        src ={'images/main.png'}></img>
+      </div>
+      <Feed
+    </>
+  )
+
+}
+
+
+export default Main
