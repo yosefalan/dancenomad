1ad8e4e18d3bceedb8d56ec193265279ad4d1535
yosefalan <yosefalan@DESKTOP-SU3N0TR.localdomain>
2021-12-18 15:32:37 -0600

    troubleshooting login issue

---
+++ .history/frontend/src/components/LandingPage/LandingPage_20211218151324.js
@@ -0,0 +1,21 @@
+import React from "react"
+import Navigation from "../Navigation/Navigation"
+import styles from "./LandingPage.module.css"
+
+
+function LandingPage() {
+  return (
+    <>
+      <Navigation />
+      <div className={styles.main}>
+        <img
+        className={styles.banner}
+        src ={'images/main2.png'}></img>
+      </div>
+    </>
+  )
+
+}
+
+
+export default LandingPage
