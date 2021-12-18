d138d76760b2e01868a126388c3e30ad7d3102a9
yosefalan <yosefalan@DESKTOP-SU3N0TR.localdomain>
2021-12-17 18:54:31 -0600

    read events

---
+++ backend/routes/api/events.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { Event } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+const { singlePublicFileUpload, singleMulterUpload } = require('../../awsS3');
+
+
+const router = express.Router();
+
+router.get('/',
+asyncHandler(async(req, res) => {
+  const events = await Event.findAll();
+  console.log("*********EVENTS:*******")
+  return res.json(events);
+}));
+
+
+
+
+
+module.exports = router;
