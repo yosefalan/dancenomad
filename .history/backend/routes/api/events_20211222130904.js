fe44b3d9be69752b5f1bdc09762fedc654af28b8
yosefalan <yosefalan@DESKTOP-SU3N0TR.localdomain>
2021-12-22 14:36:28 -0600

    create event working with single file upload

---
+++ .history/backend/routes/api/events_20211222130904.js
@@ -0,0 +1,138 @@
+const express = require("express");
+const asyncHandler = require("express-async-handler");
+const { setTokenCookie, requireAuth } = require("../../utils/auth");
+const {
+  Event,
+  Genre,
+  Event_genre,
+  Type,
+  Event_type,
+  User,
+  Venue,
+  Venue_type,
+  Venue_venue_type,
+} = require("../../db/models");
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
+const { singlePublicFileUpload, singleMulterUpload } = require("../../awsS3");
+
+const router = express.Router();
+
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const events = await Event.findAll();
+    return res.json(events);
+  })
+);
+
+router.get(
+  "/:id(\\d+)",
+  asyncHandler(async (req, res) => {
+    console.log("REQ PARAMS ID", req.params.id);
+
+    const event = await Event.findByPk(+req.params.id, {
+      include: [
+        { model: User },
+        { model: Genre, as: "event_genre" },
+        { model: Type, as: "event_type" },
+
+        { model: Venue, include: [{ model: Venue_type, as: "venue_type" }] },
+      ],
+    });
+    return res.json(event);
+  })
+);
+
+router.post(
+  "/",
+  singleMulterUpload("image"),
+  // singleMulterUpload("video"),
+  // validateSignup,
+  asyncHandler(async (req, res) => {
+    const {
+      hostId,
+      name,
+      description,
+      start_date,
+      end_date,
+      venue,
+      venue_types,
+      address,
+      city,
+      state,
+      zip,
+      country,
+      lat,
+      lng,
+      genres,
+      types,
+      image,
+      video,
+    } = req.body;
+    // const image_url = await singlePublicFileUpload(image);
+    // const video_url = await singlePublicFileUpload(video);
+    // const v = await Venue.create({
+    //   name: venue,
+    //   address,
+    //   city,
+    //   state,
+    //   zip,
+    //   country,
+    //   lat,
+    //   lng,
+    // });
+    // const venueId = v.id;
+    // const event = await Event.create({
+    //   hostId,
+    //   venueId: venueId,
+    //   name,
+    //   description,
+    //   start_date,
+    //   end_date,
+    //   // image_url,
+    //   // video_url
+    // });
+
+    // const eventId = event.id;
+    const g = JSON.parse(genres)
+    console.log(Object.values(g))
+    g.map((genre) => {
+
+      Event_genre.create({
+        eventId: 99,
+        genreId: +genre.value,
+      });
+    });
+
+    const t = JSON.parse(types)
+    console.log(Object.values(g))
+    g.map((type) => {
+
+      Event_genre.create({
+        eventId: 99,
+        genreId: +genre.value,
+      });
+    });
+
+    // await types.map((type) => {
+    //   Event_type.create({
+    //     eventId,
+    //     typeId: type.id
+    //   })
+    // })
+
+    // await venue_types.map((venue_type) => {
+    //   Venue_venue_type.create({
+    //     venueId,
+    //     venue_typeId: venue_type.id
+    //   })
+    // })
+
+    // return res.json({
+      // eventId,
+    // });
+  })
+);
+
+module.exports = router;
---
+++ .history/frontend/src/store/events_20211222134116.js
@@ -0,0 +1,145 @@
+import { csrfFetch } from './csrf';
+
+const READ_EVENTS = 'events/readEvents';
+const READ_EVENT = 'events/readEvent';
+const CREATE_EVENT = 'events/creatEvent';
+const UPDATE_EVENT = 'events/updateEvent';
+const DELETE_EVENT = 'events/deleteEvent';
+
+const readEvents = (events) => {
+  return {
+    type: READ_EVENTS,
+    payload: events,
+  };
+};
+
+const readEvent = (event) => {
+  return {
+    type: READ_EVENT,
+    payload: event,
+  };
+};
+
+const createEvent = (event) => {
+  return {
+    type: CREATE_EVENT,
+    payload: event,
+  };
+};
+
+const updateEvent = (event) => {
+  return {
+    type: UPDATE_EVENT,
+    payload: event,
+  };
+};
+
+const deleteEvent = () => {
+  return {
+    type: DELETE_EVENT,
+  };
+};
+
+export const getEvents = () => async (dispatch) => {
+
+  const res = await csrfFetch('/api/events/');
+  if(res.ok) {
+    const events = await res.json();
+    dispatch(readEvents(events));
+  };
+}
+
+export const getEvent = (id) => async (dispatch) => {
+  const res = await csrfFetch(`/api/events/${id}`);
+  if(res.ok) {
+    const event = await res.json();
+    dispatch(readEvent(event));
+  };
+}
+
+
+export const newEvent = (event) => async (dispatch) => {
+  const {
+    hostId,
+    name,
+    description,
+    start_date,
+    end_date,
+    venue,
+    venue_types,
+    address,
+    city,
+    state,
+    zip,
+    country,
+    lat,
+    lng,
+    genres,
+    types,
+    image,
+    video
+   } = event;
+   const g = JSON.stringify(genres)
+   const t = JSON.stringify(types)
+   const v = JSON.stringify(venue_types)
+   const formData = new FormData();
+   formData.append("hostId", hostId);
+   formData.append("name", name);
+   formData.append("description", description);
+   formData.append("start_date", start_date);
+   formData.append("end_date", end_date);
+   formData.append("venue", venue);
+   formData.append("venue_types", v);
+   formData.append("address", address);
+   formData.append("city", city);
+   formData.append("state", state);
+   formData.append("zip", zip);
+   formData.append("country", country);
+   formData.append("lat", lat);
+   formData.append("lng", lng);
+   formData.append("genres", g);
+   formData.append("types", t);
+   console.log("$$$$$$$$$$$$$$$$$$$", typeof g, g)
+  if (image) formData.append("image", image);
+  //  if (video) formData.append("video", video);
+
+  const res = await csrfFetch('/api/events/', {
+    method: "POST",
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+    body: formData,
+    })
+
+  if(res.ok) {
+    const event = await res.json();
+    console.log("$$$$$$ RES OK $$$$$", event)
+    dispatch(createEvent(event));
+  };
+}
+
+
+const initialState = {};
+
+const eventsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case READ_EVENTS: {
+      const allEvents = {};
+      action.payload.forEach(event => {
+        allEvents[event.id] = event
+      });
+      return {...allEvents, ...state};
+    }
+    case READ_EVENT: {
+      const event = action.payload;
+      return {[action.payload.id]: action.payload }
+    }
+    case CREATE_EVENT: {
+      const newState = {...state, [action.payload]}
+    }
+      default:
+        return state;
+      }
+    };
+
+export default eventsReducer;
